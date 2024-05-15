14101172a6d877f81f906d869c9d2afdf1895094
Sarah Skry <sarah.skrydstrup@gmail.com>
2024-05-15 13:18:50 -0400

    added useEffect to navigate from loading to the interest selection page and the home page if not successful

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../assets/motivatelogo.png';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../Welcome/Welcome.scss';
 
 
 const Loading = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const next =  location.state?.next || '/';
+      navigate(next);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate, location]); 
+
   return (
     <div className="welcome">
       <img src={logo} alt="Motivate Me Logo" className="logo" />
@@ -14,3 +27,5 @@ const Loading = () => {
 };
 
 export default Loading;
+
+
